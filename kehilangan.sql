ef53a8e6777618ebea05ac42a743347a6ac18caf
fauzialf <149127984+fauzialf@users.noreply.github.com>
2024-03-15 05:09:21 +0700

    Add files via upload

---
+++ kehilangan.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 14, 2024 at 11:08 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `android`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kehilangan`
+--
+
+CREATE TABLE `kehilangan` (
+  `IDBarang` varchar(5) NOT NULL,
+  `NamaBarang` varchar(20) NOT NULL,
+  `NamaPengajuan` varchar(20) NOT NULL,
+  `Status` varchar(10) NOT NULL,
+  `TanggalPengajuan` date NOT NULL,
+  `TanggalPengembalian` date NOT NULL,
+  `UserID` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
